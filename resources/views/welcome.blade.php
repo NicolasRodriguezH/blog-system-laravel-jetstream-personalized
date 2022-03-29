d0169f8e50b513f973b18556ff2614030492162c
Nicolas <nicolashilarion420@gmail.com>
2022-03-29 14:08:06 -0500

    Habilitando el navigation-menu jetstream

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,4 +1,8 @@
-<!DOCTYPE html>
+<x-app-layout>
+    hola estamos en prueba de desarrollo
+</x-app-layout>
+
+{{-- <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
@@ -129,4 +133,4 @@
             </div>
         </div>
     </body>
-</html>
+</html> --}}
