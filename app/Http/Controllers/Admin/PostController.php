3356c22421b3fffda18e3cae41ff81b054d52c87
Nicolas <nicolashilarion420@gmail.com>
2022-06-14 10:43:38 -0500

    added policies n worked in roles n permissions

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -82,6 +82,9 @@ class PostController extends Controller
      */
     public function edit(Post $post)
     {
+
+        $this->authorize('author', $post);
+
         /* Se agregan estas dos lineas de metodo create para vista partials.form-blade */
         $categories = Category::pluck('name', 'id');
         $tags = Tag::all();
@@ -98,6 +101,9 @@ class PostController extends Controller
      */
     public function update(PostRequest $request, Post $post)
     {
+
+        $this->authorize('author', $post);
+
         $post->update($request->all());
 
         if($request->file('file')) {
@@ -131,6 +137,9 @@ class PostController extends Controller
      */
     public function destroy(Post $post)
     {
+
+        $this->authorize('author', $post);
+
         $post->delete();
 
         return redirect()->route('admin.posts.index')->with('info', 'El post se elimino con exito');
