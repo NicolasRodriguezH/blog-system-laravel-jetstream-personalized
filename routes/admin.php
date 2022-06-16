d146cd8018b7511c1a3f07035e8596ec34ccbb0c
Nicolas <nicolashilarion420@gmail.com>
2022-06-16 18:16:52 -0500

    working in modal-table-users

--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\HomeController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\TagController;
 use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -11,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('', [HomeController::class, 'index'])->name('admin.home');
 
 // Resources para CRUDS de Admin
+Route::resource('users', UserController::class)->names('admin.users');
 Route::resource('categories', CategoryController::class)->names('admin.categories');
 Route::resource('tags', TagController::class)->names('admin.tags');
 Route::resource('posts', PostController::class)->names('admin.posts');
