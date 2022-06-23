75d4e18c0a506ac7a5e74a04c8e5b63b407f1ce2
Nicolas <nicolashilarion420@gmail.com>
2022-06-23 18:53:10 -0500

    crud of roles in process

--- config/adminlte.php
+++ config/adminlte.php
@@ -258,7 +258,12 @@ return [
             'icon'        => 'fas fa-users fa-fw',
             'can'       => 'admin.users.index'
         ],
-        
+        [
+            'text'        => 'Lista de roles',
+            'route'         => 'admin.roles.index',
+            'icon'        => 'fas fa-users-cog fa-fw',
+            /* 'can'       => 'admin.users.index' */
+        ],
         [
             'text' => 'Categorias',
             'route'  => 'admin.categories.index',
---
+++ resources/views/admin/roles/create.blade.php
@@ -0,0 +1,51 @@
+@extends('adminlte::page')
+
+@section('title', 'Pagina Laravel')
+
+@section('content_header')
+    <h1>Crear nuevo rol</h1>
+@stop
+
+@section('content')
+    <div class="card">
+        <div class="card-body">
+
+            {!! Form::open(['route' => 'admin.roles.store']) !!}
+                <div class="form-group">
+                    {!! Form::label('name', 'Nombre') !!}
+                    {!! Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Ingrese el nombre del rol']) !!}
+
+                    @error('name')
+                        <small class="text-danger">
+                            {{$message}}
+                        </small>
+                    @enderror
+
+                </div>
+
+                <h2 class="h3">Listado de permisos</h2>
+
+                @foreach ($permissions as $permission)
+                    <div>
+                        <label>
+                            {!! Form::checkbox('permissions[]', $permission->id, null, ['class' => 'mr-1']) !!}
+                            {{$permission->description}}
+                        </label>
+                    </div>
+                @endforeach
+
+
+                {!! Form::submit('Crear rol', ['class' => 'btn btn-primary']) !!}
+            {!! Form::close() !!}
+            
+        </div>
+    </div>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop
